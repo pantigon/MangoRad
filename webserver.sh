cd49f68caaef831957c439e61a6061d8a82b6f07
pantigon <47517210+pantigon@users.noreply.github.com>
2021-10-31 02:51:50 +0700

    Fix php7.2 on Ubuntu 20.04

--- webserver.sh
+++ webserver.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 echo ">>> Installing WebServer <<<"
-sudo apt-get install -y apache2 php7.2 php7.2-curl php7.2-mbstring php7.2-xml php7.2-gd php7.2-dev php-pear php7.2-ssh2 libmcrypt-dev mcrypt make
+sudo apt install -y apache2 php7.2 php7.2-curl php7.2-mbstring php7.2-xml php7.2-gd php7.2-dev php-pear php7.2-ssh2 libmcrypt-dev mcrypt make
 sleep 2
 sudo pecl install mcrypt-1.0.2
 echo -ne '\n'
