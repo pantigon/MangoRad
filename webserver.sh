c9996462de5f92054e9eac2170c50a9518951b2c
pantigon <47517210+pantigon@users.noreply.github.com>
2021-10-30 22:29:41 +0700

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo ">>> Installing WebServer <<<"
-sudo apt-get install -y apache2 php php-curl php-mbstring php-xml php-gd php-dev php-pear php-ssh2 libmcrypt-dev mcrypt make
+sudo apt-get install -y apache2 php7.2 php7.2-curl php7.2-mbstring php7.2-xml php7.2-gd php7.2-dev php-pear php7.2-ssh2 libmcrypt-dev mcrypt make
 sleep 2
-sudo pecl install mcrypt-1.0.1
+sudo pecl install mcrypt-1.0.2
 echo -ne '\n'
 echo ">>> Finished Installing WebServer"
 sleep 2
